c59cde2f9a687d9118f8049719bf390e832012fc
Mangesh Aher <mangeshaherngp@gmail.com>
2024-04-13 18:35:59 +0530

    ll two pointers

---
+++ dsaAlgo/src/twopointers/LLInteresectionEasy.java
@@ -0,0 +1,32 @@
+package twopointers;
+
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+*/
+public class LLInteresectionEasy {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a!=b){
+            if(a==null){
+                a = headB;// We are making both pointers a and b reach same point dista + distb = distb + dista
+            }
+            else{
+                a = a.next;
+            }
+            if(b==null){
+                b = headA;// We are making both pointers a and b reach same point dista + distb = distb + dista
+            }
+            else{
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
---
+++ dsaAlgo/src/twopointers/ListNode.java
@@ -0,0 +1,13 @@
+package twopointers;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+      ListNode(int val, ListNode next) {
+          this.val = val; this.next = next;
+      }
+}
